da4150f0f3f11e03f3e7b042fa262a472ccb226d
Ryan Leary <ryanleary@gmail.com>
2017-09-13 02:05:59 -0400

    Make pytorch 0.2 compat and add weight_norm

--- model.py
+++ model.py
@@ -4,6 +4,7 @@ from collections import OrderedDict
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torch.nn.utils import weight_norm
 
 supported_rnns = {
     'lstm': nn.LSTM,
@@ -47,22 +48,22 @@ class InferenceBatchLogSoftmax(nn.Module):
 
 
 class BatchRNN(nn.Module):
-    def __init__(self, input_size, hidden_size, rnn_type=nn.LSTM, bidirectional=False, batch_norm=True):
+    def __init__(self, input_size, hidden_size, rnn_type=nn.LSTM, bidirectional=False, weight_norm=True):
         super(BatchRNN, self).__init__()
         self.input_size = input_size
         self.hidden_size = hidden_size
         self.bidirectional = bidirectional
-        self.batch_norm = SequenceWise(nn.BatchNorm1d(input_size)) if batch_norm else None
         self.rnn = rnn_type(input_size=input_size, hidden_size=hidden_size,
                             bidirectional=bidirectional, bias=False)
+        if self.weight_norm:
+            self.rnn = weight_norm(self.rnn)
+
         self.num_directions = 2 if bidirectional else 1
 
     def forward(self, x):
-        if self.batch_norm is not None:
-            x = self.batch_norm(x)
         x, _ = self.rnn(x)
         if self.bidirectional:
-            x = x.view(x.size(0), x.size(1), 2, -1).sum(2).view(x.size(0), x.size(1), -1)  # (TxNxH*2) -> (TxNxH) by sum
+            x = x.view(x.size(0), x.size(1), 2, -1).sum(2, keepdim=True).view(x.size(0), x.size(1), -1)  # (TxNxH*2) -> (TxNxH) by sum
         self.rnn.flatten_parameters()
         return x
 
@@ -102,7 +103,7 @@ class DeepSpeech(nn.Module):
 
         rnns = []
         rnn = BatchRNN(input_size=rnn_input_size, hidden_size=rnn_hidden_size, rnn_type=rnn_type,
-                       bidirectional=bidirectional, batch_norm=False)
+                       bidirectional=bidirectional, weight_norm=True)
         rnns.append(('0', rnn))
         for x in range(nb_layers - 1):
             rnn = BatchRNN(input_size=rnn_hidden_size, hidden_size=rnn_hidden_size, rnn_type=rnn_type,
